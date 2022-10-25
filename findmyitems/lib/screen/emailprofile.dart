973ef2503f51ba9c59229263aa996c3a2e6856a9
RatchanonBusaracome <64030165@kmitl.ac.th>
2022-10-24 21:55:02 -0700

    editing email profile sceen <RS on ten labtop>

--- findmyitems/lib/screen/emailprofile.dart
+++ findmyitems/lib/screen/emailprofile.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:findmyitems/model/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,7 +16,19 @@ class _EmailProfileScreenState extends State<EmailProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Profile"),centerTitle: true,),
-      
+      /*body: StreamBuilder(
+        stream: FirebaseFirestore.instance?.collection("Profile").snapshots(),
+        builder: (context,AsyncSnapshot<QuerySnapshot> snapshot){
+          if (!snapshot.hasData){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          return ListView(children: [
+            snapshot.data.docs.map((document) {
+              return Container(child: ListView,)
+            })
+          ],);
+        },
+        ),*/
     );
   }
 }
